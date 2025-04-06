31ead620215548baffcf4a51176b6de53799ccbc
Hassane <bezzahassane@gmail.com>
2025-04-06 22:34:33 +0200

    add comoposon reparation telephone et formatage telephone

--- src/components/Avis.tsx
+++ src/components/Avis.tsx
@@ -5,7 +5,14 @@ import "swiper/css/autoplay";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "./Avis.scss";
-import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from "react-icons/fa";
+import {
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
 import "./SocialMedia.scss";
 
 interface AvisClient {
@@ -23,8 +30,12 @@ const avisClients: AvisClient[] = [
 const AvisClients: React.FC = () => {
   return (
     <div className="avis-container">
-      <h1 className="main-title">Avis de nos clients sur les réseaux sociaux</h1>
-      <h2 className="sub-title">Découvrez ce que nos clients pensent de nos services.</h2>
+      <h1 className="main-title">
+        Avis de nos clients sur les réseaux sociaux
+      </h1>
+      <h2 className="sub-title">
+        Découvrez ce que nos clients pensent de nos services.
+      </h2>
 
       <Swiper
         spaceBetween={30}
@@ -52,25 +63,56 @@ const AvisClients: React.FC = () => {
       <section className="social-media-section py-6">
         <div className="container mx-auto text-center max-w-4xl px-4">
           <h3 className="text-lg font-bold text-white mb-4">
-            SUIVEZ-NOUS SUR LES <span className="font-extrabold">RÉSEAUX SOCIAUX</span>
+            SUIVEZ-NOUS SUR LES{" "}
+            <span className="font-extrabold">RÉSEAUX SOCIAUX</span>
           </h3>
           <div className="flex justify-center space-x-6">
-            <a href="https://x.com/has_svc" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-300">
+            <a
+              href="https://x.com/has_svc"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-2xl hover:text-gray-300"
+            >
               <FaTwitter />
             </a>
-            <a href="https://www.facebook.com/people/Has-Tech-Services/61571620113575/" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-300">
+            <a
+              href="https://www.facebook.com/people/Has-Tech-Services/61571620113575/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-2xl hover:text-gray-300"
+            >
               <FaFacebook />
             </a>
-            <a href="https://www.instagram.com/has.tech.svc/?hl=en" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-300">
+            <a
+              href="https://www.instagram.com/has.tech.svc/?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-2xl hover:text-gray-300"
+            >
               <FaInstagram />
             </a>
-            <a href="https://www.linkedin.com/in/has-tech-svc-10bb27345/" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-300">
+            <a
+              href="https://www.linkedin.com/in/has-tech-svc-10bb27345/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-2xl hover:text-gray-300"
+            >
               <FaLinkedin />
             </a>
-            <a href="https://www.tiktok.com/@has.tech.svc" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-300">
+            <a
+              href="https://www.tiktok.com/@has.tech.svc"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-2xl hover:text-gray-300"
+            >
               <FaTiktok />
             </a>
-            <a href="https://www.youtube.com/@HasTechServices" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-300">
+            <a
+              href="https://www.youtube.com/@HasTechServices"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white text-2xl hover:text-gray-300"
+            >
               <FaYoutube />
             </a>
           </div>
