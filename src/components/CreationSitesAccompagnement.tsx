c0797a2d71e3ca6f0bee5ed96b9becc0a48c2ac2
Hassane <bezzahassane@gmail.com>
2025-05-06 14:54:23 +0200

    add image

--- src/components/CreationSitesAccompagnement.tsx
+++ src/components/CreationSitesAccompagnement.tsx
@@ -22,7 +22,7 @@ const CreationSitesAccompagnement: React.FC = () => {
           Nous créons des sites vitrines professionnels pour les entreprises ou les particuliers. Notre approche est personnalisée pour chaque projet afin de garantir que votre site reflète parfaitement l'image de votre entreprise et répond à vos besoins spécifiques.
         </p>  
         <img
-          src="https://i.imgur.com/bfzF9iE.jpeg"
+          src="https://i.imgur.com/9ccnvTb.jpeg"
           alt="Conception de sites vitrines"
           className="image-small"
         />
@@ -35,7 +35,7 @@ const CreationSitesAccompagnement: React.FC = () => {
           Tous nos sites sont conçus pour être entièrement responsive, c'est-à-dire adaptés à tous les types d'appareils : ordinateurs, tablettes, et smartphones. Un site bien conçu doit offrir une expérience utilisateur optimale, peu importe la taille de l'écran.
         </p>
         <img
-          src="https://i.imgur.com/rBV4Cqo.jpeg"
+          src="https://i.imgur.com/NUxPhkX.jpeg"
           alt="Design responsive"
           className="image-small"
         />
@@ -48,7 +48,7 @@ const CreationSitesAccompagnement: React.FC = () => {
           Nous vous aidons à élaborer une stratégie efficace pour augmenter votre présence en ligne. Que ce soit sur Facebook, Instagram, LinkedIn ou d'autres réseaux sociaux, nous gérons vos comptes et créons des contenus engageants pour attirer plus de clients.
         </p>
         <img
-          src="https://i.imgur.com/ZJPmgio.jpeg"
+          src="https://i.imgur.com/ys3K6Br.jpeg"
           alt="Réseaux sociaux"
           className="image-small"
         />
@@ -61,7 +61,7 @@ const CreationSitesAccompagnement: React.FC = () => {
           Nos services incluent l'optimisation pour les moteurs de recherche (SEO). Nous vous aidons à améliorer la visibilité de votre site sur Google, en vous assurant qu'il apparaît dans les résultats de recherche pertinents pour vos mots-clés cibles.
         </p>
         <img
-          src="https://i.imgur.com/IvWvk8e.jpeg"
+          src="https://i.imgur.com/GnjqdoQ.jpeg"
           alt="SEO et optimisation"
           className="image-small"
         />
@@ -74,7 +74,7 @@ const CreationSitesAccompagnement: React.FC = () => {
           Après la création de votre site, nous offrons un service de maintenance pour nous assurer que votre site reste toujours à jour et fonctionne parfaitement. Nous offrons également un support continu pour répondre à toutes vos questions ou préoccupations.
         </p>
         <img
-          src="https://i.imgur.com/SrQj0Z5.jpeg"
+          src="https://i.imgur.com/Zy0480Z.jpeg"
           alt="Maintenance et support"
           className="image-small"
         />
